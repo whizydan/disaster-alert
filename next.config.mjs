d6647db32e6b600a0f985955bdf2b69f49186aaf
kerberos <whizydan@gmail.com>
2024-12-03 20:54:30 +0300

    disabled type checking

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    // Allows production builds to succeed even with ESLint errors
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
+  },
+};
 
 export default nextConfig;
